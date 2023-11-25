02ec5d68798a3fea1dc0aa93fe886c311c5f1241
unknown <hnazila71@gmail.com>
2023-11-25 20:57:44 +0700

    hasan

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('daftar');
+        return view('ppdb_mars');
     }
     public function hello(): string
     {
